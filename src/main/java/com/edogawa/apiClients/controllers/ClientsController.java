56a9524004f02fbcb681b5ab486e01ea774d5542
EdogawaCoder <edogawa.araujo@gmail.com>
2025-10-31 12:55:03 -0300

    feat: add search by name like function on controller

--- src/main/java/com/edogawa/apiClients/controllers/ClientsController.java
+++ src/main/java/com/edogawa/apiClients/controllers/ClientsController.java
@@ -1,7 +1,25 @@
 package com.edogawa.apiClients.controllers;
 
+import com.edogawa.apiClients.entities.Client;
+import com.edogawa.apiClients.repositories.ClientRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/clients")
 public class ClientsController {
 
+    @GetMapping
+    public List<Client> getAll(@RequestParam String name) throws Exception {
 
+
+        var clientRepository = new ClientRepository();
+
+        return clientRepository.searchClients(name);
+    }
 
 }
